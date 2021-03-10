a4c4489578b74e498f088625db3ef98b644af3f5
Howard Otuya <howardotuya17@gmail.com>
2021-03-10 14:34:10 +0100

    Basic Component and Routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-function App() {
+//We're bringing the PrivateRoute
+
+import PrivateRoute from './components/routing/PrivateRoute';
+
+//We're bringing the screen components
+
+import PrivateScreen from './components/screens/PrivateScreen';
+import LoginScreen from './components/screens/Loginscreen';
+import RegisterScreen from './components/screens/Registerscreen';
+import ForgotPasswordScreen from './components/screens/ForgotPasswordscreen';
+import ResetPasswordScreen from './components/screens/ResetPasswordscreen';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+       <div className='App'>
+         <Switch>
+         <PrivateRoute exact path='/' component={PrivateScreen} />
+         <Route exact path='/login' component={LoginScreen} />
+         <Route exact path='/register' component={RegisterScreen} />
+         <Route exact path='/forgotpassword' component={ForgotPasswordScreen} />
+         <Route exact path='/passwordreset/:resetToken' component={ResetPasswordScreen} />
+           
+         </Switch>
+       </div>
+     </Router>
   );
 }
 
